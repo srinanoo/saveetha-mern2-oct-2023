3c1fbb3eb898fda94fe561a4413e3586f724789f
srinanoo <srinanoo@gmail.com>
2023-10-13 17:48:56 +0530

    13-10-2023

--- controllers/traineesController.js
+++ controllers/traineesController.js
@@ -2,11 +2,16 @@ const fs = require('fs');
 const traineeFile = "./trainees.json";
 
 function getAllTrainees(req, res) {
-    fs.readFile(traineeFile, 'utf8', (err, data) => {
-        if(err) res.send(err);
-
-        res.send(data);
-    });
+    try {
+        fs.readFile(traineeFile, 'utf8', (err, data) => {
+            if(err) res.status(500).json({"message": err});
+    
+            // res.send(data);
+            res.status(200).json(JSON.parse(data));
+        });
+    } catch(err) {
+        res.status(500).json({"message": err.message});
+    }
 }
 
 function getTrainee(req, res) {
@@ -24,15 +29,15 @@ function getTrainee(req, res) {
         });
 
         if(results.length > 0)
-            res.send(results);
+            res.status(200).json(JSON.parse(results));
         else 
-            res.send("No Trainees Found!");
+            res.status(402).json({"message": "No Trainees Found!"});
     });
 }
 
 const addTrainee = (req, res) => {
     fs.readFile(traineeFile, 'utf8', (err, data) => {
-        if(err) res.send(err);
+        if(err) res.status(500).json({"error": err});
 
         let existingTrainees = JSON.parse(data);
         let newTrainee = req.body;
@@ -50,22 +55,74 @@ const addTrainee = (req, res) => {
             }
         }
 
-        (flag) ? res.send("Trainee Already Exists!") : existingTrainees.push(newTrainee);
+        (flag) ? res.status(403).json({"message": "Trainee Already Exists!"}) : existingTrainees.push(newTrainee);
 
         fs.writeFile(traineeFile, JSON.stringify(existingTrainees), (err) => {
-            if(err) res.send(err);
+            if(err) res.status(500).json({"message": err});
 
-            res.send("Trainee Added Successfully!");
+            res.status(200).json({"message": "Trainee Added Successfully!"});
         });
     });
 };
 
 const updateTrainee = (req, res) => {
-    res.send("Update Trainee");
+    try {
+        fs.readFile(traineeFile, 'utf8', (err, data) => {
+            if(err) res.status(500).json({"error": err});
+    
+            let existingTrainees = JSON.parse(data);
+            let updateTrainee = req.body;
+
+            // for(let i = 0; i < existingTrainees.length; i++) {
+            //     if(existingTrainees[i].id === updateTrainee.id) {
+            //         existingTrainees[i] = {...existingTrainees[i], ...updateTrainee}
+            //     }
+            // }
+
+            let finalTrainees = [];
+            for(let i = 0; i < existingTrainees.length; i++) {
+                if(existingTrainees[i].id === updateTrainee.id)
+                    finalTrainees[i] = updateTrainee;
+                else
+                    finalTrainees.push(existingTrainees[i]);
+            }
+            fs.writeFile(traineeFile, JSON.stringify(finalTrainees), (err) => {
+                if(err) res.status(500).json({"message": err});
+    
+                res.status(200).json({"message": "Trainee Updated Successfully!"});
+            });
+        });
+    } catch (err) {
+        res.status(500).json({"message": err.message});
+    }
 };
 
 const deleteTrainee = (req, res) => {
-    res.send(`Delete Trainee: ${req.params.id}`);
+    try {
+        let traineeId = req.params.id;
+        if(traineeId) {
+            fs.readFile(traineeFile, 'utf8', (err, data) => {
+                if(err) res.status(500).json({"error": err});
+        
+                let existingTrainees = JSON.parse(data);
+                let trainees = existingTrainees.filter(v => v.id != traineeId);
+
+                if(trainees.length < existingTrainees.length) {
+                    fs.writeFile(traineeFile, JSON.stringify(trainees), (err) => {
+                        if(err) res.status(500).json({"message": err});
+            
+                        res.status(200).json({"message": "Trainee Deleted Successfully!"});
+                    });
+                } else {
+                    res.status(402).json({"message": "No Trainee found!"});
+                }
+            });
+        } else {
+            res.status(400).json({"message": "Please enter a trainee id"});
+        }
+    } catch(err) {
+        res.status(500).json({"message": err.message});
+    }
 };
 
 module.exports = {
